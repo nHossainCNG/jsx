b30752d9b3c0d2a3e84d2baa58aef2128d78f83b
nhossainCNG <nhossain@axcess-financial.com>
2020-06-05 16:12:53 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+
+
 const App = function (){
-    return <div>Hi there!</div>;
+        return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a className="avatar" href= "/">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">Sam</a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text"> Wow! Nice Post!</div>
+                </div>
+            </div>
+        </div>
+        );
 }
 
 
